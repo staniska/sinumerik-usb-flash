10e3d9d0a6524b82b52613ef747b8d31220ddff2
Staniska <a3boot.babaev@gmail.com>
2024-03-20 17:52:41 +0300

    Initial commit

---
+++ spec/sinumerik-usb-flash-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SinumerikUsbFlashView from '../lib/sinumerik-usb-flash-view';
+
+describe('SinumerikUsbFlashView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
